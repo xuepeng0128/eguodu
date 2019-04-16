0946e989d5267b1c2daa40639ef7c62cfcd78632
yxpmatrix <2426574593@qq.com>
2019-04-16 22:03:37 +0800

    new

---
+++ src/main/java/com/yxp/eguodu/entity/CircleClass.java
@@ -0,0 +1,14 @@
+package com.yxp.eguodu.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CircleClass {
+    private String  circleClassId;
+    private String  pareClassId;
+    private String circleClassName;
+}
