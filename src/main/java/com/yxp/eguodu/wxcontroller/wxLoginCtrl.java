ee9c1a2ecd055be0108503f8f2867d020d0f9067
yxpmatrix <2426574593@qq.com>
2019-05-29 22:23:23 +0800

    new

---
+++ src/main/java/com/yxp/eguodu/wxcontroller/wxLoginCtrl.java
@@ -0,0 +1,19 @@
+package com.yxp.eguodu.wxcontroller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(value="微信登录",tags={"微信登录webapi接口"})
+@CrossOrigin
+@RestController
+@RequestMapping(value = "/api/wxlogin",  produces = "application/json;charset=UTF-8")
+public class wxLoginCtrl {
+
+
+
+
+
+
+}
